182f4b00e49bc3d92dc64d2f9cea2e8b541d4cb7
Asrin Dayananda <adayanan@deakin.edu.au>
2021-07-08 20:16:33 +0100

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -16,18 +16,29 @@ from utils import u, html_unescape, filter_json_index_by_year
 
 
 json_index_content = {}
+
+# Azcodez
 facebook_page_id = os.environ.get('FACEBOOK_PAGE_ID')
-facebook_page_id_motivateym = os.environ.get('FACEBOOK_PAGE_ID_MOTIVATEYM')
 facebook_access_token = os.environ.get('FACEBOOK_ACCESS_TOKEN')
-facebook_access_token_mym = os.environ.get('FACEBOOK_ACCESS_TOKEN_MYM')
 facebook_api_end = 'https://graph.facebook.com/{0}/feed'.format(facebook_page_id)
 facebook_api_photo_end = 'https://graph.facebook.com/{0}/photos'.format(facebook_page_id)
-facebook_api_end_mym = 'https://graph.facebook.com/{0}/feed'.format(facebook_page_id_motivateym)
+
+# MYM
+facebook_page_id_mym = os.environ.get('FACEBOOK_PAGE_ID_MYM')
+facebook_access_token_mym = os.environ.get('FACEBOOK_ACCESS_TOKEN_MYM')
+facebook_api_end_mym = 'https://graph.facebook.com/{0}/feed'.format(facebook_page_id_mym)
+
+# Feed url - Hashcode
 feed_url = os.environ.get('FEED_URL')
 feed_data = parse_rss_bytes(urlopen(feed_url).read())
 
 current_timestamp = int(datetime.now().strftime('%Y%m%d%H%M%S'))
 current_hour = int(datetime.now().strftime('%H'))
+
+# If in below timeslot post
+# if current_hour not in [9, 13, 15, 22]:
+#     print('Script wasnt called in a recommended hour. Aborting.')
+#     sys.exit(0)
 
 for post in feed_data.items:
     post_timestamp = post.pub_date.strftime('%Y%m%d%H%M%S')
@@ -72,10 +83,10 @@ http_request_mym = Request(url=facebook_api_end_mym, method='POST',
                        data=urlencode(facebook_api_data_mym).encode())
 
 # Photo post
-facebook_api_data_photo = {'url': 'https://azcodez.com/images/150633b813614aa8b24cd8459fcf0b21.png',
-                     'access_token': facebook_access_token}
-http_request_photo = Request(url=facebook_api_photo_end, method='POST',
-                       data=urlencode(facebook_api_data_photo).encode())
+# facebook_api_data_photo = {'url': 'https://azcodez.com/images/150633b813614aa8b24cd8459fcf0b21.png',
+#                      'access_token': facebook_access_token}
+# http_request_photo = Request(url=facebook_api_photo_end, method='POST',
+#                        data=urlencode(facebook_api_data_photo).encode())
 
 # Chairmans post
 # Get few urls and shuffle them and post
@@ -88,6 +99,7 @@ while count < 6:
         # result = json.loads(str(urlopen(http_request_two).read(), 'utf-8'))
         # time.sleep(60)
         # result = json.loads(str(urlopen(http_request_photo).read(), 'utf-8'))
+        # MYM
         result = json.loads(str(urlopen(http_request_mym).read(), 'utf-8'))
     except Exception as e:
         print('There was an error publishing: {0}'.format(e))
