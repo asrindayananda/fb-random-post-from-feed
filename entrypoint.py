fea1dbecf708a78ad5c6d111c9c65fd0ac72db59
Asrin Dayananda <adayanan@deakin.edu.au>
2021-07-18 19:37:07 +0100

    Test with azcodez posting code to see if its a syntax thing

--- entrypoint.py
+++ entrypoint.py
@@ -15,10 +15,10 @@ from utils import u, html_unescape, filter_json_index_by_year
 json_index_content = {}
 
 # Azcodez
-facebook_page_id = os.environ.get('FACEBOOK_PAGE_ID')
-facebook_access_token = os.environ.get('FACEBOOK_ACCESS_TOKEN')
-facebook_api_end = 'https://graph.facebook.com/{0}/feed'.format(facebook_page_id)
-facebook_api_photo_end = 'https://graph.facebook.com/{0}/photos'.format(facebook_page_id)
+# facebook_page_id = os.environ.get('FACEBOOK_PAGE_ID')
+# facebook_access_token = os.environ.get('FACEBOOK_ACCESS_TOKEN')
+# facebook_api_end = 'https://graph.facebook.com/{0}/feed'.format(facebook_page_id)
+# facebook_api_photo_end = 'https://graph.facebook.com/{0}/photos'.format(facebook_page_id)
 
 # MYM
 facebook_page_id_mym = os.environ.get('FACEBOOK_PAGE_ID_MYM')
@@ -29,6 +29,9 @@ facebook_api_end_mym = 'https://graph.facebook.com/{0}/feed'.format(facebook_pag
 facebook_page_id_motivateNotes = os.environ.get('FACEBOOK_PAGE_ID_MOTIVATENOTES')
 facebook_access_token_motivateNotes = os.environ.get('FACEBOOK_ACCESS_TOKEN_MOTIVATENOTES')
 facebook_api_photo_end_motivateNotes = 'https://graph.facebook.com/{0}/photos'.format(facebook_page_id_motivateNotes)
+facebook_page_id = os.environ.get('FACEBOOK_PAGE_ID_MOTIVATENOTES')
+facebook_access_token = os.environ.get('FACEBOOK_ACCESS_TOKEN_MOTIVATENOTES')
+facebook_api_end = 'https://graph.facebook.com/{0}/feed'.format(facebook_page_id)
 
 # Feed url - Hashcode
 feed_url = os.environ.get('FEED_URL')
@@ -63,12 +66,12 @@ random_post_url = u('{0}#{1}'.format(
     json_index_filtered[random_post_id]['url'],
     current_timestamp))
 
-# Azcodez
-facebook_api_data = {'message': random_post_title,
-                     'link': random_post_url,
-                     'access_token': facebook_access_token}
-http_request = Request(url=facebook_api_end, method='POST',
-                       data=urlencode(facebook_api_data).encode())
+# Azcodez WORKS
+# facebook_api_data = {'message': random_post_title,
+#                      'link': random_post_url,
+#                      'access_token': facebook_access_token}
+# http_request = Request(url=facebook_api_end, method='POST',
+#                        data=urlencode(facebook_api_data).encode())
 # facebook_api_data_two = {'message': 'Testing',
 #                      'link': random_post_url,
 #                      'access_token': facebook_access_token}
@@ -76,10 +79,10 @@ http_request = Request(url=facebook_api_end, method='POST',
 #                        data=urlencode(facebook_api_data_two).encode()) 
 
 # MYM post
-facebook_api_data_mym = {'message': 'Motivation',
-                        'link': 'https://youtu.be/tnWR1wMxFWs',
-                        'access_token': facebook_access_token_mym}
-http_request_mym = Request(url=facebook_api_end_mym, method='POST', data=urlencode(facebook_api_data_mym).encode())
+# facebook_api_data_mym = {'message': 'Motivation',
+#                         'link': 'https://youtu.be/tnWR1wMxFWs',
+#                         'access_token': facebook_access_token_mym}
+# http_request_mym = Request(url=facebook_api_end_mym, method='POST', data=urlencode(facebook_api_data_mym).encode())
 
 # AzCodez Photo post
 # facebook_api_data_photo = {'url': 'https://azcodez.com/images/150633b813614aa8b24cd8459fcf0b21.png',
@@ -88,10 +91,15 @@ http_request_mym = Request(url=facebook_api_end_mym, method='POST', data=urlenco
 #                        data=urlencode(facebook_api_data_photo).encode())
 
 # Motivate Notes Photo post
-facebook_api_data_photo_motivateNotes = {'url': 'https://azcodez.com/images/150633b813614aa8b24cd8459fcf0b21.png',
-                                         'access_token': facebook_access_token_motivateNotes}
-http_request_photo_motivateNotes = Request(url=facebook_api_photo_end_motivateNotes, method='POST',
-                                           data=urlencode(facebook_api_data_photo_motivateNotes).encode())
+# facebook_api_data_photo_motivateNotes = {'url': 'https://azcodez.com/images/150633b813614aa8b24cd8459fcf0b21.png',
+#                                          'access_token': facebook_access_token_motivateNotes}
+# http_request_photo_motivateNotes = Request(url=facebook_api_photo_end_motivateNotes, method='POST',
+#                                            data=urlencode(facebook_api_data_photo_motivateNotes).encode())
+facebook_api_data = {'message': random_post_title,
+                     'link': random_post_url,
+                     'access_token': facebook_access_token}
+http_request = Request(url=facebook_api_end, method='POST',
+                       data=urlencode(facebook_api_data).encode())
 
 # Chairmans post
 # Get few urls and shuffle them and post
@@ -99,20 +107,20 @@ http_request_photo_motivateNotes = Request(url=facebook_api_photo_end_motivateNo
 # TIH
 
 # Azcodes post
-# count = 0
-# while count < 1:
-#     try:
-#         result = json.loads(str(urlopen(http_request).read(), 'utf-8'))
-#         #time.sleep(3)
-#     except Exception as e:
-#         print('There was an error publishing: {0}'.format(e))
-#         count += 1
-#         continue
-#     if 'error' in result:
-#         count += 1
-#         continue
-#     print('Successfully published!: {0}'.format(random_post_url))
-#     break
+count = 0
+while count < 1:
+    try:
+        result = json.loads(str(urlopen(http_request).read(), 'utf-8'))
+        #time.sleep(3)
+    except Exception as e:
+        print('There was an error publishing: {0}'.format(e))
+        count += 1
+        continue
+    if 'error' in result:
+        count += 1
+        continue
+    print('Successfully published!: {0}'.format(random_post_url))
+    break
 
 # MYM post
 # count = 0
@@ -131,17 +139,17 @@ http_request_photo_motivateNotes = Request(url=facebook_api_photo_end_motivateNo
 #     break
 
 # Motivate Notes Photo post
-count = 0
-while count < 1:
-    try:
-        # time.sleep(60)
-        result = json.loads(str(urlopen(http_request_photo_motivateNotes).read(), 'utf-8'))
-    except Exception as e:
-        print('There was an error publishing: {0}'.format(e))
-        count += 1
-        continue
-    if 'error' in result:
-        count += 1
-        continue
-    print('Successfully published photo!: ')
-    break
+# count = 0
+# while count < 1:
+#     try:
+#         # time.sleep(60)
+#         result = json.loads(str(urlopen(http_request_photo_motivateNotes).read(), 'utf-8'))
+#     except Exception as e:
+#         print('There was an error publishing: {0}'.format(e))
+#         count += 1
+#         continue
+#     if 'error' in result:
+#         count += 1
+#         continue
+#     print('Successfully published photo!: ')
+#     break
